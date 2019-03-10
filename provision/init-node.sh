56ad0c5e94746dbb07355db9732800d8b7f0dd09
Jonas Kello <jonas.kello@gmail.com>
2019-03-10 11:41:45 +0100

    fix

--- provision/init-node.sh
+++ provision/init-node.sh
@@ -37,9 +37,6 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-# sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-# deb http://apt.kubernetes.io/ kubernetes-xenial main
-# EOF
 sudo apt-get update
 
 # Install kubelet, kubeadm, kubectl
