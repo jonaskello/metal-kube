f884179f87bdd97d43f424ffabb024fe010aae2d
Jonas Kello <jonas.kello@gmail.com>
2019-03-10 11:28:12 +0100

    Add subo

--- provision/init-node.sh
+++ provision/init-node.sh
@@ -34,7 +34,7 @@ sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Add repos for kubelet, kubeadm, kubectl
 sudo apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
