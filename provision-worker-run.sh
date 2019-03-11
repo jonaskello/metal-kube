ca39dd65942e2d91487fe5a90eea97dce27fa4b4
Jonas Kello <jonas.kello@gmail.com>
2019-03-11 23:14:06 +0100

    Params

--- provision-worker-run.sh
+++ provision-worker-run.sh
@@ -3,5 +3,15 @@
 # 2. Version of kubernetes binaries to install
 # 3. The join command
 
+# Verify arguments
+if [ -z "$3" ]
+  then
+    echo "The argument for join command was not supplied."
+fi
+
+echo "Param1: $1"
+echo "Param2: $2"
+echo "Param3: $3"
+
 # Run init
-curl -fsSL https://raw.githubusercontent.com/jonaskello/metal-kube/master/init-node.sh | bash
+curl -fsSL https://raw.githubusercontent.com/jonaskello/metal-kube/master/init-node.sh | bash -s -- $1 $2
