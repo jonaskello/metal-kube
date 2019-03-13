c08c8763c7b7f520cbe44201975e9227d19099e4
Jonas Kello <jonas.kello@gmail.com>
2019-03-13 23:12:20 +0100

    cat

--- add-master-get.sh
+++ add-master-get.sh
@@ -3,14 +3,14 @@
 # Set options to fail properly
 exec 4>&1
 BASH_XTRACEFD=4
-set -Eeuxo pipefail
+set -Eeuo pipefail
 export SHELLOPTS
 
 # Pack all certs etc. that needs to be copied to the new master node
 temp_dir=$(mktemp -d)
 trap "{ rm -rf $temp_dir; }" EXIT
-pushd /etc/kubernetes
+cd /etc/kubernetes
 sudo tar -czf $temp_dir/certs.tar.gz pki/ca.crt pki/ca.key pki/sa.key pki/sa.pub pki/front-proxy-ca.crt pki/front-proxy-ca.key pki/etcd/ca.crt pki/etcd/ca.key admin.conf
-tar -ztvf $temp_dir/certs.tar.gz
-ls $temp_dir
-popd
+# tar -ztvf $temp_dir/certs.tar.gz
+# ls $temp_dir
+cat $temp_dir/certs.tar.gz
